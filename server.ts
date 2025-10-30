d907bc7ea0b8aa34a97895eef1ceeccd3aedab8d
Raunit <raunitarya599@gmail.com>
2025-10-31 00:13:07 +0530

    Setup express server

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import app from './src/app.js';
+import app from './src/app.ts';
 
 const startServer = () => {
     const port = process.env.PORT || 3000;
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express from 'express';
 
 const app = express();
 
-
+// Routes 
+app.get('/', (_req: express.Request, res: express.Response) => {
+    res.json({message: "Welcome to API"})
+})
 
 export default app;
