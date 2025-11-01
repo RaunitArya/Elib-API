d9f89fc3311d57cb0f1e380144ec6fa06742a79d
Raunit <raunitarya599@gmail.com>
2025-11-01 23:29:22 +0530

    Add login endpoint

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -5,6 +5,8 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { config } from "../config/config.ts";
 import { type User } from "./userTypes.ts";
+import userRouter from "./userRouter.ts";
+import { create } from "domain";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -45,9 +47,45 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const token = jwt.sign({ sub: newUser._id }, config.jwtSecret as string, {
       expiresIn: "7d",
     });
-  // Response
-  res.status(201).json({ accessToken: token });
+    // Response
+    res.status(201).json({ accessToken: token });
   } catch (error) {
     return next(createHttpError(500, "Error while signing JWT token"));
   }
 };
+
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(createHttpError(400, "All fields are required"));
+  }
+
+  let user: User | null;
+  try {
+    user = await userModel.findOne({ email: email });
+    if (!user) {
+      return next(createHttpError(404, "User not found"));
+    }
+  } catch (error) {
+    return next(createHttpError(400, "Error while getting user"));
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return next(createHttpError(400, "User name and password incorrect"));
+  }
+
+  // Create access token
+  try {
+    const token = jwt.sign({ sub: user._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+    });
+    res.json({ accesToken: token });
+  } catch (error) {
+    return next(createHttpError(500, "Error while fetching JWT token"));
+  }
+};
+
+export { createUser, loginUser };
