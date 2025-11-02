8a47594c97844f3cf17fe0dd385c7873e22aef32
Raunit <raunitarya599@gmail.com>
2025-11-02 10:30:06 +0530

    Refactor upload middleware & fix upload dest path

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -6,15 +6,17 @@ import multer from 'multer';
 const bookRouter = express.Router();
 
 const upload = multer({
-    dest: path.resolve(__dirname, ' ../../public/data/uploads'),
+    dest: path.dirname(' ../../public/data/uploads'),
     limits: {fileSize: 3e7} 
 })
 
-// Routes
-bookRouter.post('/', upload.fields([
+const uploadMiddleware = upload.fields([
     {name: "coverImage", maxCount: 1},
     {name: "file", maxCount:1},
-]), createBook); 
+])
+
+// Routes
+bookRouter.post('/', uploadMiddleware, createBook); 
 bookRouter.post('/read', readBook);
 bookRouter.post('/update', updateBook);
 bookRouter.post('/delete', deleteBook);
